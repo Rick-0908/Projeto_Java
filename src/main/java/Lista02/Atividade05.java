76739fcae255b40b51e749663200f5dbf6433f21
Miguel Walisson <miguelwalisson098@gmail.com>
2025-09-29 14:56:03 -0300

    "New Exercices"

---
+++ src/main/java/Lista02/Atividade05.java
@@ -0,0 +1,16 @@
+package Lista02;
+
+import java.util.Scanner;
+
+public class Atividade05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Olá, digite o seu sálario para receber o calculo de 10% de acressimo: ");
+        double salario = sc.nextDouble();
+
+        double aumento = (salario/100)*10;
+        double resultado = salario + aumento;
+
+        System.out.println("O seu aumento é de: " + resultado );
+    }
+}
---
+++ src/main/java/Lista02/Atividade08.java
@@ -0,0 +1,19 @@
+package Lista02;
+
+import java.util.Scanner;
+
+public class Atividade08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu 1 número inteiro: ");
+        int n1 = sc.nextInt();
+        System.out.println("Digite seu 2 número inteiro: ");
+        int n2 = sc.nextInt();
+        int div = n1 / n2;
+        System.out.println("Seu resultado inteiro deu: " +div);
+
+        double div2 = div/2;
+        System.out.println("A divisão de resto foi: "+div2);
+
+    }
+}
